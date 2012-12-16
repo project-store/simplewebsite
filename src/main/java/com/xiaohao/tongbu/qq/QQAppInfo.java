41addd45713ce0c85dadc200f5a7e628a4249bed
xiaohao <zipu405@126.com>
2012-12-16 14:06:35 +0800

    add page

---
+++ src/main/java/com/xiaohao/tongbu/qq/QQAppInfo.java
@@ -0,0 +1,17 @@
+package com.xiaohao.tongbu.qq;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: xiqohao
+ * Date: 12-12-8
+ * Time: 下午3:12
+ * To change this template use File | Settings | File Templates.
+ */
+public class QQAppInfo {
+    public static String appKey="801064960";
+    public static String appSecret="1f202127c7d4d51e7897ea88d3cdbd22";
+    public static String appName="万能同步";
+    public static String firstUrl="https://open.t.qq.com/cgi-bin/oauth2/authorize";
+    public static String secondUrl="https://open.t.qq.com/cgi-bin/oauth2/access_token";
+    public static String appReqUrl="http://localhost:8080/qqaction";
+}
---
+++ src/main/java/com/xiaohao/tongbu/sina/SinaAppInfo.java
@@ -0,0 +1,11 @@
+package com.xiaohao.tongbu.sina;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: xiqohao
+ * Date: 12-12-8
+ * Time: 下午3:13
+ * To change this template use File | Settings | File Templates.
+ */
+public class SinaAppInfo {
+}
