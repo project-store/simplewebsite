7343ab8e9ff05cdf176d3c9adf9610a10df9de70
xiaohao <zipu405@126.com>
2013-04-04 11:59:30 +0800

    add random view list

--- src/main/java/com/xiaohao/base/util/BootStrap.java
+++ src/main/java/com/xiaohao/base/util/BootStrap.java
@@ -3,6 +3,7 @@ package com.xiaohao.base.util;
 import com.xiaohao.base.model.AdminUser;
 import com.xiaohao.core.dao.UserDao;
 import com.xiaohao.core.service.AdminUserService;
+import com.xiaohao.core.service.RandomWIshService;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.annotation.PostConstruct;
@@ -17,6 +18,8 @@ public class BootStrap {
     private UserDao userDao;
     @Autowired
     private AdminUserService adminUserService;
+    @Autowired
+    private RandomWIshService randomWIshService;//初始化随即显示的list
     @PostConstruct
     public void init(){
         //初始化管理员配置
