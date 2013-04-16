0f8c72db9ce431fc93a4aa030fcc7149b5df25ee
haozipu <zipu405@126.com>
2013-04-16 14:31:06 +0800

    fix issuse 1

---
+++ src/main/java/com/xiaohao/base/model/ShareConfig.java
@@ -0,0 +1,56 @@
+package com.xiaohao.base.model;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: SuperHao
+ * Date: 13-4-16
+ * Time: 下午1:28
+ * To change this template use File | Settings | File Templates.
+ */
+public class ShareConfig extends BaseModel {
+    private Integer configId;
+    private String url;
+    private String title;
+    private String summry;
+    private String picUrl;
+
+    public Integer getConfigId() {
+        return configId;
+    }
+
+    public void setConfigId(Integer configId) {
+        this.configId = configId;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSummry() {
+        return summry;
+    }
+
+    public void setSummry(String summry) {
+        this.summry = summry;
+    }
+
+    public String getPicUrl() {
+        return picUrl;
+    }
+
+    public void setPicUrl(String picUrl) {
+        this.picUrl = picUrl;
+    }
+}
--- src/main/java/com/xiaohao/core/action/InitAction.java
+++ src/main/java/com/xiaohao/core/action/InitAction.java
@@ -1,12 +1,17 @@
 package com.xiaohao.core.action;
 
 import com.opensymphony.xwork2.ActionSupport;
+import com.xiaohao.base.model.IndexScrollPic;
+import com.xiaohao.core.service.IndexScrollPicService;
 import org.apache.struts2.convention.annotation.Action;
 import org.apache.struts2.convention.annotation.Actions;
 import org.apache.struts2.convention.annotation.ParentPackage;
 import org.apache.struts2.convention.annotation.Result;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
+
+import java.util.List;
 
 /**
  * Created with IntelliJ IDEA.
@@ -21,8 +26,30 @@ import org.springframework.stereotype.Controller;
 @Actions({ @Action(value = ( "/initAction" ), results = {
         @Result(name = "index", location = "/WEB-INF/page/index.jsp") }) })
 public class InitAction extends ActionSupport {
+    @Autowired
+    IndexScrollPicService indexScrollPicService;
+    private String flag;
+    private List<IndexScrollPic> indexScrollPicList;
     @Override
     public String execute() throws Exception {
+        indexScrollPicList = indexScrollPicService.loadAllIndexPic();
+        flag="index";
         return "index";
+    }
+
+    public String getFlag() {
+        return flag;
+    }
+
+    public void setFlag(String flag) {
+        this.flag = flag;
+    }
+
+    public List<IndexScrollPic> getIndexScrollPicList() {
+        return indexScrollPicList;
+    }
+
+    public void setIndexScrollPicList(List<IndexScrollPic> indexScrollPicList) {
+        this.indexScrollPicList = indexScrollPicList;
     }
 }
