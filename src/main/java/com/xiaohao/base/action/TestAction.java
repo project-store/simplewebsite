dd37abc6fb165262407a540067d0f967c7c0c124
haozipu <zipu405@126.com>
2013-05-30 18:40:40 +0800

    add sae jar

---
+++ src/main/java/com/xiaohao/base/action/TestAction.java
@@ -0,0 +1,15 @@
+package com.xiaohao.base.action;
+import com.sina.sae.storage.SaeStorage;
+/**
+ * Created with IntelliJ IDEA.
+ * User: SuperHao
+ * Date: 13-5-30
+ * Time: 下午6:22
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestAction {
+    public void main(){
+        SaeStorage saeStorage = new SaeStorage();
+
+    }
+}
